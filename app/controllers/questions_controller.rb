cebe692fd60d046e7257995e738bfa4a14ffcdba
Josenilton Duarte <n8zion@gmail.com>
2022-11-15 10:44:34 -0300

    recover

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,7 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @answer = params[:question]
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
   # Defines the root path route ("/")
   # root "articles#index"
 end
